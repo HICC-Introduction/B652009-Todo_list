889c3d693317d5b3f0ef8709d53817c44c5add56
naro-kim <gracias9022@gmail.com>
2021-04-01 23:42:39 +0900

    Css 정리

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 const todoInput=document.querySelector('.todo-input'); //변수 todoInput은 Input form을 가리킨다.
 const addButton=document.querySelector('.add-btn'); //변수 addButton은 Input form 내부의 add버튼을 가리킨다.
 const todoList=document.querySelector('.todo-list'); //변수 todoList는 addButton click 이후에 생성될 todo-list의 부모인 <ul>tag를 가리킨다.
+const delMode=document.querySelector('.del-btn'); //변수 delMode는 delete button을 가리킨다.
+const getCategory=document.getElementById("category-select").value;
 
 //EventListners
 addButton.addEventListener('click',addTodo);
+//addButton.addEventListener('click',getCategory);
+
 
 function lineCheck(){
     var chk = document.getElementsByClassName('todo-item');
     chk.classList.add("done");
-    console.log('click');
+    li.appendChild(chk);
 }
+
+// Edit mode and Delete mode shoulb in front of addTodo function
+
 
 //Functions
 function addTodo(event){
@@ -28,13 +35,13 @@ function addTodo(event){
  
     //create <li> with class "todo-item" under <div> todo
     const newTodo = document.createElement('li');
-    newTodo.innerText='input text';
+    newTodo.innerText=todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo); //li가 div에 붙어있게 하기
     //create <filter>
     const categoryLabel = document.createElement('span');  //html에서 category-select option으로 받은 값 가져와야 함
     categoryLabel.classList.add('category-item');
-    categoryLabel.innerText ="selected Category";
+    categoryLabel.innerText = getCategory;
     todoDiv.appendChild(categoryLabel);
 
     //append to list
@@ -42,13 +49,4 @@ function addTodo(event){
 }
 
 
-
-newFunction();
-
-function newFunction() {
-        window.onload(function () {
-            var itemStyle = localStorage.getItem('done_chk');
-            var as = document.getElementById("done_chk");
-            as.classList.add(itemStyle);
-        });
-    }
+//button click -> checked 상태로 변화 
